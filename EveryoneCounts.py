6f14b232bd30c33cb00f381b137f055ed336c2c0
Michael Adams <github@mmmad.de>
2020-04-04 20:12:48 +0100

    Matomo javascript

--- EveryoneCounts.py
+++ EveryoneCounts.py
@@ -99,7 +99,27 @@ st.markdown("""
         }
     </style>
     """, unsafe_allow_html=True)
-
+    
+ # tracking javascript
+st.markdown("""   
+    <!-- Matomo -->
+    <script type="text/javascript">
+    alert("bla")
+      var _paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["disableCookies"]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//matomo.everyonecounts.de/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Matomo Code -->
+""", unsafe_allow_html=True)
 
 # sidebar footer
 st.sidebar.markdown("""
