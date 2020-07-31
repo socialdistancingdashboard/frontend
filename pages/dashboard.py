b32b692f37b87263955ed7ca4d4e4f449e8dc061
Julius <42575200+BihJ@users.noreply.github.com>
2020-07-31 02:32:28 +0200

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -31,10 +31,7 @@ def load_topojson():
 @st.cache(ttl=43200) # time-to-live: 12h
 def load_real_data():
     response = requests.get('https://im6qye3mc3.execute-api.eu-central-1.amazonaws.com/prod')
-    try:
-        jsondump = response.json()["body"]
-    except:
-        st.text(response.text)
+    jsondump = response.json()["body"]
     county_names, county_ids,_,_ = load_topojson()
     id_to_name = {cid:county_names[idx] for idx,cid in enumerate(county_ids)}
     
